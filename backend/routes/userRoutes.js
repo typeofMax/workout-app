513b80848c633be24e55f00537191049ac14f1d3
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-03 22:52:17 +0300

    add user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUserProfile } from '../controllers/userController.js';
+import { getUserProfile } from '../controllers/user/profileController.js';
+import { userRegistration } from '../controllers/user/regController.js';
 
 const router = express.Router()
 
 router.route('/profile').get(getUserProfile);
-
+router.route('/').post(userRegistration);
 
 export default router;

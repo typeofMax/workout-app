18ba5c8981af48b54c70f728eeffbb4ff67dea6d
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-04 13:09:13 +0300

    feat: :sparkles: Add the login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
+
+import { userAuthentication } from '../controllers/user/authController.js';
 import { getUserProfile } from '../controllers/user/profileController.js';
 import { userRegistration } from '../controllers/user/regController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
-router.route('/profile').get(getUserProfile);
+router.route('/profile').get(protect, getUserProfile);
+router.route('/login').post(userAuthentication);
 router.route('/').post(userRegistration);
 
 export default router;

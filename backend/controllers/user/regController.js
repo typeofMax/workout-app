0bc619f780675acdeac5c930b81ec9439b1a60ca
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-04 13:06:48 +0300

    feat: :sparkles: Add a generated token to response

--- backend/controllers/user/regController.js
+++ backend/controllers/user/regController.js
@@ -1,23 +1,26 @@
 import User from '../../models/userModel.js';
 import asyncHandler from 'express-async-handler';
+import { generateToken } from '../../helper/generateToken.js';
 
 //@descr User Registration
 //@routes POST api/users
 //@access public
-export const userRegistration = asyncHandler( async (req, res) => {
-    const { email, password } =  req.body;
+export const userRegistration = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
     const isRegistered = await User.findOne({ email });
 
     if (isRegistered) {
-        res.status(400)
+        res.status(400);
         throw new Error('Пользователь с таким email уже зарегистрирован');
     }
 
     const user = await User.create({
-        email, 
+        email,
         password,
     });
 
-    res.json(user);
+    const token = generateToken(user._id);
+
+    res.json({ user, token });
 });

1a759701e4b1cac3c8ac8fe56e34b34dc17ac447
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-12 19:19:39 +0300

    feat: Create workout log model

---
+++ backend/models/workoutLogModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Schema;
+
+const workoutLogSchema = mongoose.Schema(
+    {
+        user: {
+            type: ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        workout: {
+            type: ObjectId,
+            ref: 'Workout',
+            required: true,
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        minimize: false,
+        timestamps: true,
+    }
+);
+
+const WorkoutLog = mongoose.model('Workout', workoutLogSchema);
+
+export default WorkoutLog;

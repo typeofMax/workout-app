ba03eb8cabf40471f1e700e342cba258418f4d1f
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-12 19:18:57 +0300

    refactor: Change the image to imageId in Schema
    Now use the imageId in the schema of the exercise model, because we will store pictures in an array

--- backend/models/exerciseModel.js
+++ backend/models/exerciseModel.js
@@ -10,8 +10,8 @@ const exerciseSchema = mongoose.Schema(
             type: Number,
             required: true,
         },
-        image: {
-            type: String,
+        imageId: {
+            type: Number,
             required: true,
         },
     },

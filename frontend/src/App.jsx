a82cbc09c5c09ffb9627287c4dd3575c9ae97649
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-13 22:48:10 +0300

    feat: Creat App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+//@Components
+import Layout from './components/common/Layout';
+import Home from './components/pages/Home/Home';
+
+const App = () => {
+    return (
+        <Layout>
+            <Home />
+        </Layout>
+    );
+};
+
+export default App;

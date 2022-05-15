837f5541ccda208269841d3cf13ed07ffe22b3b8
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-15 19:37:39 +0300

    feat: :sparkles: Dynamic background style change
    Change background dynamically based on current location

--- frontend/src/components/common/Layout.jsx
+++ frontend/src/components/common/Layout.jsx
@@ -1,16 +1,25 @@
+//@Libs
+import cn from 'classnames';
+import { useLocation } from 'react-router-dom';
 //@components
 import Header from './Header/Header';
-
 //@Styles
 import styles from './Layout.module.scss';
 
-const Layout = ({ children }) => {
-    return (
-        <div className={styles.wrapper}>
-            <Header />
-            {children}
-        </div>
-    );
+const Layout = ({ children, bgImage, height = '356px' }) => {
+  let { pathname } = useLocation();
+
+  return (
+    <div
+      className={cn(styles.wrapper, {
+        [styles['other-page']]: pathname !== '/',
+      })}
+      style={{ backgroundImage: `url(${bgImage})`, height: height }}
+    >
+      <Header />
+      <div className={styles.container}>{children}</div>
+    </div>
+  );
 };
 
 export default Layout;

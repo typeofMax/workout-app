9c9f899af612f8b10d27ec6e26d2656423528eae
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-15 20:18:57 +0300

    feat: Make a transition by click
    Make a transition to the page for adding a workout by clicking on the button

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -1,3 +1,5 @@
+//@Libs
+import { useNavigate } from 'react-router-dom';
 //@Components
 import Counters from '../../common/Counters/Counters';
 import Layout from '../../common/Layout';
@@ -7,10 +9,13 @@ import styles from './Home.module.scss';
 //@Images
 import bgImage from '../../../images/home-bg.jpg';
 
+
 const Home = () => {
+  let navigate = useNavigate();
+  
   return (
     <Layout bgImage={bgImage} height='100%'>
-      <Button text='New' viewStyle='main' />
+      <Button text='New' viewStyle='main' onClick={() => navigate('/new-workout')}/>
       <h1 className={styles.heading}>SHOULDERS EXERCISES</h1>
       <Counters />
     </Layout>

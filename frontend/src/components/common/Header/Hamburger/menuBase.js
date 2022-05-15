a6fb417774215f5c8995b64a0ff86513827c65c3
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-15 19:43:19 +0300

    Create base menu routes

---
+++ frontend/src/components/common/Header/Hamburger/menuBase.js
@@ -0,0 +1,14 @@
+export const menuBase = [
+  {
+    title: 'Workouts',
+    path: '/workouts',
+  },
+  {
+    title: 'Create new',
+    path: '/new-workout',
+  },
+  {
+    title: 'Profile',
+    path: '/profile',
+  },
+];

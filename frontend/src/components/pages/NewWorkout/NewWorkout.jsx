f27532b65015f82cfdceabd813af7e57ae114fdd
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-15 21:19:00 +0300

    feat: :sparkles: Use custom select field

--- frontend/src/components/pages/NewWorkout/NewWorkout.jsx
+++ frontend/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -8,6 +8,7 @@ import styles from './NewWorkout.module.scss';
 import bgImage from '../../../images/new-workout-bg.jpg';
 import Input from '../../ui/Input/Input';
 import Button from '../../ui/Button/Button';
+import SelectInput from '../../ui/SelectInput/SelectInput';
 
 const NewWorkout = () => {
   const [name, setName] = useState('');
@@ -25,7 +26,7 @@ const NewWorkout = () => {
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
-          {/* React select */}
+          <SelectInput/>
           <Button text='Create' />
         </form>
       </div>

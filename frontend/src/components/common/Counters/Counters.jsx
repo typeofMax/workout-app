c4830434795d1f0f41aa3d9ef02835a585e206a6
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-18 00:57:29 +0300

    feat: Display counter values based on data from props

--- frontend/src/components/common/Counters/Counters.jsx
+++ frontend/src/components/common/Counters/Counters.jsx
@@ -1,25 +1,30 @@
 //@Styles
+import PropTypes from 'prop-types';
 import styles from './Counters.module.scss';
 
-const times = {
-  minutes: 20,
-  level: 'hard',
-  up: '5%',
-};
-
-const Counters = () => {
+const Counters = ({ minutes, workouts, kgs }) => {
   return (
     <div className={styles.wrapper}>
-      {Object.entries(times).map((item) => {
-        return (
-          <div className={styles.item} key={'_id' + item[0]}>
-            <h3 className={styles.title}>{item[0]}</h3>
-            <div className={styles.value}>{item[1]}</div>
-          </div>
-        );
-      })}
+      <div className={styles.item}>
+        <h3 className={styles.title}>Minutes</h3>
+        <div className={styles.value}>{minutes}</div>
+      </div>
+      <div className={styles.item}>
+        <h3 className={styles.title}>Workouts</h3>
+        <div className={styles.value}>{workouts}</div>
+      </div>
+      <div className={styles.item}>
+        <h3 className={styles.title}>Kgs</h3>
+        <div className={styles.value}>{kgs}</div>
+      </div>
     </div>
   );
 };
 
 export default Counters;
+
+Counters.propTypes = {
+  minutes: PropTypes.number,
+  workouts: PropTypes.number,
+  kgs: PropTypes.number,
+};

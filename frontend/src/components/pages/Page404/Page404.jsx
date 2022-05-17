0e2d0a4ce342122cbfd06f51f7e5145be5cb8492
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-18 01:00:46 +0300

    Create 404 page

---
+++ frontend/src/components/pages/Page404/Page404.jsx
@@ -0,0 +1,12 @@
+//@Components
+import Layout from '../../common/Layout';
+//@Images
+import bgImage from '../../../images/auth-bg.jpg';
+
+const Page404 = () => {
+  return (
+    <Layout bgImage={bgImage} height='260px' heading='Page not found' />
+  )
+};
+
+export default Page404;

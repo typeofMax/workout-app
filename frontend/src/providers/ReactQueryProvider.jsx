cfed13755915fcaeda8fad3feaee0e63c77b20b2
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-17 15:24:07 +0300

    refactor: Move the react query provider to a separate file

---
+++ frontend/src/providers/ReactQueryProvider.jsx
@@ -0,0 +1,18 @@
+//@Libs
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+//@Components
+import AppProvider from './AppProvider';
+
+const queryClient = new QueryClient();
+
+const ReactQueryProvider = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppProvider />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default ReactQueryProvider

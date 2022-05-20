8654bdb22103afcd91b6664de1cdd88412950218
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-20 11:11:42 +0300

    feat: Add the ability to receive exercises and create a workout

--- frontend/src/pages/NewWorkout/NewWorkout.jsx
+++ frontend/src/pages/NewWorkout/NewWorkout.jsx
@@ -1,11 +1,12 @@
 ///@Libs
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useQuery, useMutation } from 'react-query';
 //@Components
+import { Alert, Button, Input, Loader, SelectInput } from '../../components/ui';
 import Layout from '../../components/common/Layout';
-import Input from '../../components/ui/Input/Input';
-import Button from '../../components/ui/Button/Button';
-import SelectInput from '../../components/ui/SelectInput/SelectInput';
+//@Helpers
+import { $api } from '../../core/api/api';
 //@Styles
 import styles from './NewWorkout.module.scss';
 //@Images
@@ -13,24 +14,73 @@ import bgImage from '../../assets/images/new-workout-bg.jpg';
 
 const NewWorkout = () => {
   const [name, setName] = useState('');
+  const [currentExercises, setCurrentExercises] = useState([]);
+
+  const { data } = useQuery(
+    'Get exercises',
+    () =>
+      $api({
+        url: '/exercises',
+      }),
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const {
+    mutate: addWorkout,
+    isSuccess,
+    error,
+    isLoading,
+  } = useMutation(
+    'Add workout',
+    (idS) =>
+      $api({
+        url: '/workouts',
+        method: 'POST',
+        body: { name, exercisesIds: idS },
+      }),
+    {
+      onSuccess() {
+        setName('');
+        setCurrentExercises([]);
+      },
+    }
+  );
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const idS = currentExercises.map((exercise) => exercise.value);
+    addWorkout(idS);
+  };
 
   return (
     <>
       <Layout bgImage={bgImage} height='260px' heading='Create new workout' />
       <div className={styles.wrapper}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler}>
           <Input
             type='text'
             placeholder='Name'
             value={name}
             onChange={(e) => setName(e.target.value)}
+            required
           />
           <Link className={styles['new-exercise']} to='/new-exercise'>
             Add new exercise
           </Link>
-          <SelectInput />
+          <SelectInput
+            data={data}
+            value={currentExercises}
+            onChange={setCurrentExercises}
+            required
+          />
           <Button text='Create' />
         </form>
+        {isLoading && <Loader />}
+        {isSuccess && <Alert text='Successfully created' />}
+        {error && <Alert type='error' text={error} />}
       </div>
     </>
   );

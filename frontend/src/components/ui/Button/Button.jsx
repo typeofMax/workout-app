b1612cde75b5bc70d82b88d4e576b4a237fe07e3
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-20 11:09:10 +0300

    feat: Add prop types checking

--- frontend/src/components/ui/Button/Button.jsx
+++ frontend/src/components/ui/Button/Button.jsx
@@ -1,13 +1,20 @@
+//@Libs
+import PropTypes from 'prop-types';
 //@Styles
 import styles from './Button.module.scss';
 
-const Button = ({ text, viewStyle = 'purple', ...props}) => {
-
-    return (
-      <button className={styles[viewStyle]} {...props}>
-        {text}
-      </button>
-    );
+const Button = ({ text, viewStyle = 'purple', ...props }) => {
+  return (
+    <button className={styles[viewStyle]} {...props}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
+
+Button.propTypes = {
+  text: PropTypes.string,
+  viewStyle: PropTypes.string,
+  props: PropTypes.object,
+};

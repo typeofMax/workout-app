3f0617973c8c55d97d7baadac0671de2ef159ad1
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-20 11:08:27 +0300

    feat: Group ui components to one line import

---
+++ frontend/src/components/ui/index.jsx
@@ -0,0 +1,7 @@
+import Alert from './Alert/Alert';
+import Button from './Button/Button';
+import Input from './Input/Input';
+import Loader from './Loader/Loader';
+import SelectInput from './SelectInput/SelectInput';
+
+export { Alert, Button, Input, Loader, SelectInput };

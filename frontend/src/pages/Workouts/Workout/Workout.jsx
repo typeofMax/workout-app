559eb08e135f76d5b9ad4c5b352603dedff9b06b
typeofMax <64499072+typeofMax@users.noreply.github.com>
2022-05-20 23:33:00 +0300

    Create workout page

--- frontend/src/pages/Workouts/Workout/Workout.jsx
+++ frontend/src/pages/Workouts/Workout/Workout.jsx
@@ -1,9 +1,11 @@
 //@Libs
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useQuery, useMutation } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import cn from 'classnames';
 //@Components
 import Layout from '../../../components/common/Layout';
-import Loader from '../../../components/ui/Loader/Loader';
+import { Loader, Alert } from '../../../components/ui';
 //@Helpers
 import { $api } from '../../../core/api/api';
 //@Styles
@@ -11,51 +13,90 @@ import styles from './Workout.module.scss';
 //@Images
 import bgImage from '../../../assets/images/profile-bg.jpg';
 
-const Workout = () => {
-  const { id: workoutId } = useParams();
+const SingleWorkout = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const {
-    data: workout,
-    isSuccess,
-    isLoading,
-  } = useQuery(
-    'Get workout',
+  const { data, isSuccess, isLoading } = useQuery('get workout', () =>
+    $api({
+      url: `/workouts/log/${id}`,
+    })
+  );
+
+  const { mutate: setWorkoutCompleted, error: errorCompleted } = useMutation(
+    'Change log state',
     () =>
       $api({
-        url: `workouts/${workoutId}`,
+        url: '/workouts/log/completed',
+        method: 'PUT',
+        body: { logId: id },
       }),
     {
-      refetchOnWindowFocus: false,
+      onSuccess() {
+        navigate(`/workouts`);
+      },
     }
   );
+
+  useEffect(() => {
+    if (
+      isSuccess &&
+      data?.exerciseLogs &&
+      data.exerciseLogs.length ===
+        data.exerciseLogs.filter((log) => log.completed).length &&
+      data._id === id
+    ) {
+      setWorkoutCompleted();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.exerciseLogs]);
 
   return (
     <div className='container'>
       <Layout bgImage={bgImage} height='260px'>
-        <div className={styles.head}>
-          {isSuccess && workout && (
-            <>
-              <time className={styles.head__time}>{workout.minutes} min</time>
-              <h1 className={styles.head__title}>{workout.name}</h1>
-            </>
-          )}
-        </div>
+        {isSuccess && (
+          <div className={styles.head}>
+            <time className={styles.head__time}>{data.minutes + ' min.'}</time>
+            <h1 className={styles.head__title}>{data.workout.name}</h1>
+          </div>
+        )}
       </Layout>
+
       <div className={styles.wrapper}>
-        <ul className={styles.list}>
-          {isLoading && <Loader />}
-          {isSuccess &&
-            workout &&
-            workout.exercises.map(({_id, name, imageName}) => (
-              <li className={styles.list__item} key={_id}>
-                <p>{name}</p>
-                <img src={`/uploads/${imageName}.svg`} alt={imageName} />
-              </li>
-            ))}
-        </ul>
+        <div style={{ width: '90%', margin: '0 auto' }}>
+          {errorCompleted && <Alert type='error' text={errorCompleted} />}
+        </div>
+        {isLoading || (isSuccess && data._id !== id) ? (
+          <Loader />
+        ) : (
+          <ul className={styles.list}>
+            {data.exerciseLogs.map((exLog, idx) => {
+              return (
+                <li
+                  className={cn(styles['list__item'], {
+                    [styles.completed]: exLog.completed,
+                  })}
+                  onClick={() => navigate(`/exercise/${exLog._id}`)}
+                  key={`${data._id}${idx}`}
+                >
+                  <span>{exLog.exercise.name}</span>
+                  <img
+                    src={`/uploads/${exLog.exercise.imageName}.svg`}
+                    height='34'
+                    alt=''
+                    draggable={false}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+        )}
+        {isSuccess && data?.length === 0 && (
+          <Alert type='warning' text='Exercises not found' />
+        )}
       </div>
     </div>
   );
 };
 
-export default Workout;
+export default SingleWorkout;
